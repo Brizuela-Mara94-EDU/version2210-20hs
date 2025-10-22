2d138173974b5ba3479b54e65d5a5eb2817089f8
Marcela Mancini <marcelamancinidiaz@gmail.com>
2025-10-22 12:41:37 -0300

    build final con .gitattributes y modelos actualizados

--- vite.config.js
+++ vite.config.js
@@ -2,31 +2,25 @@ import { defineConfig } from 'vite';
 import basicSsl from '@vitejs/plugin-basic-ssl';
 import { resolve } from 'path';
 
-// Usamos una función para que la configuración sea dinámica
 export default defineConfig(({ command }) => {
-  // Si estás ejecutando 'npm run dev', el comando es 'serve'
   if (command === 'serve') {
-    // --- CONFIGURACIÓN PARA DESARROLLO LOCAL ---
     return {
-      // No definimos 'base', por lo que usará la raíz '/' por defecto.
-      plugins: [
-        basicSsl() // Necesario para HTTPS y la cámara
-      ],
-      server: {
-        host: true
-      }
+      plugins: [basicSsl()],
+      server: { host: true }
     };
   } else {
-    // --- CONFIGURACIÓN PARA PRODUCCIÓN (GitHub Pages) ---
-    // Si estás ejecutando 'npm run build', el comando es 'build'
     return {
-      base: '/AppMindAr/',
+      base: '/AppMindAr/', // tu repo en GitHub Pages
       build: {
+        outDir: 'docs',      // <- build final va a docs/
         rollupOptions: {
           input: {
             main: resolve(__dirname, 'index.html'),
             dino: resolve(__dirname, 'dino.html'),
-            minas: resolve(__dirname, 'minas.html')
+            minas: resolve(__dirname, 'minas.html'),
+            elements: resolve(__dirname, 'elements.html'),
+            scanner: resolve(__dirname, 'scanner.html'),
+            'arjs-scanner': resolve(__dirname, 'arjs-scanner.html')
           }
         }
       }
